52b7ecf33deee91c04371d3e6e4d68fce3b2331e
admin <0_0phantom0_0@mail.ru>
2021-03-17 21:56:49 +0300

    Add registration form, and some style

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,41 @@
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Post
+
+
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+        widgets = {
+            "title": forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Название поста'
+            }),
+            "text": forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Текст статьи'
+            })
+        }
+
+class SignUp(UserCreationForm):
+    password1 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Повторите пароль'}))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+        widgets = {
+            "username": forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите логин'
+            }),
+            "email": forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите email'
+            }),
+        }
--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from .forms import SignUp
 from django.shortcuts import redirect
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render (request, 'blog/post_detail.html', {'post':post})
+
 def post_new(request):
     form = PostForm()
     if request.method == "POST":
@@ -25,16 +28,30 @@ def post_new(request):
     else:
         form = PostForm()
     return render (request, 'blog/post_edit.html', {'form': form})
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render (request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post =get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect ('/')
+
+def sign_up(request):
+    form = SignUp()
+    if request.method == 'POST':
+        form = SignUp(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    return render(request, 'blog/sign_up.html', {'form': form})
